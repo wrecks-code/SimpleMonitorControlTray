494fcade4304f3e5f4d800abdc2b7293da139f8f
Marius Ladner <fap5wreck@gmail.com>
2024-04-16 19:37:58 +0200

    Update SimpleMonitorControlTray.py

--- SimpleMonitorControlTray.py
+++ SimpleMonitorControlTray.py
@@ -2,6 +2,11 @@ import smct_pkg.config as config
 import smct_pkg.tray as tray
 
 
+# TODO add selection of monitor at startup
+# TODO clean up imports like so:
+# import smct_pkg.config
+
+
 def main():
     config.read_config()
     tray.init_tray()
