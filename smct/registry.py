ae97d937d923caee41d406051739ac4ae72b9d01
wreck-code <fap5wreck@gmail.com>
2024-04-18 13:57:17 +0200

    refactoring

--- smct/registry.py
+++ smct/registry.py
@@ -1,5 +1,4 @@
 import winreg
-
 from smct import paths, ui_strings
 
 KEY_NAME = ui_strings.APP_NAME
@@ -8,24 +7,25 @@ REGISTRY_KEY = r"Software\Microsoft\Windows\CurrentVersion\Run"
 
 def is_autostartkey_in_registry():
     try:
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REGISTRY_KEY)
-        _ = winreg.QueryValueEx(key, KEY_NAME)
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REGISTRY_KEY, 0, winreg.KEY_READ)
+        value = winreg.QueryValueEx(key, KEY_NAME)
         winreg.CloseKey(key)
-        return True
+        return value is not None
     except FileNotFoundError:
         return False
 
 
 def add_to_autostart():
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REGISTRY_KEY, 0, winreg.KEY_WRITE)
-    winreg.SetValueEx(
-        key,
-        KEY_NAME,
-        0,
-        winreg.REG_SZ,
-        f'"{paths.EXE_PATH[0].upper() + paths.EXE_PATH[1:]}"',
-    )
-    winreg.CloseKey(key)
+    try:
+        key = winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, REGISTRY_KEY, 0, winreg.KEY_WRITE
+        )
+        exe_path = paths.EXE_PATH[0].upper() + paths.EXE_PATH[1:]
+        winreg.SetValueEx(key, KEY_NAME, 0, winreg.REG_SZ, exe_path)
+        winreg.CloseKey(key)
+    except PermissionError:
+        # print(f"Error occurred while adding to autostart: {e}")
+        pass
 
 
 def remove_from_autostart():
